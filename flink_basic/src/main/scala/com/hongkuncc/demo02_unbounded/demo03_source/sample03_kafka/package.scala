ad4675c0a418627e7a79db6b0ab7c5aa4f2e64ed
hongkuncc <hongkuncc@163.com>
2020-04-08 01:23:02 +0800

    initial

---
+++ flink_basic/src/main/scala/com/hongkuncc/demo02_unbounded/demo03_source/sample03_kafka/package.scala
@@ -0,0 +1,10 @@
+package com.hongkuncc.demo02_unbounded.demo03_source
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+package object ReadDataFromKafkaDemo extends App {
+  //1、环境
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+
+}
